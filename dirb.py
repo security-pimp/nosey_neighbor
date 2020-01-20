2f10aefae56c3fe2720fd54250c9b8bb98a1c42e
security PIlVlP <the.security.pimp@gmail.com>
2020-01-20 15:32:55 -0700

    Added Option For HTTP Port Number

--- dirb.py
+++ dirb.py
@@ -3,11 +3,11 @@
 import sys, os, time
 target = str(sys.argv[1])
 
-def dirb_http():
-	cmd2 = 'dirb http://' + target + ' -r |tee dirb_http.txt'
+def dirb_http(http_port):
+	cmd2 = 'dirb http://' + target + ':' + http_port + ' -r |tee dirb_http.txt'
 	os.system(cmd2)
 
-def dirb_https():
-	cmd3 = 'dirb https://' + target + ' -r |tee dirb_https.txt'
+def dirb_https(https_port):
+	cmd3 = 'dirb https://' + target + ':' + https_port + ' -r |tee dirb_https.txt'
 	os.system(cmd3)
 
