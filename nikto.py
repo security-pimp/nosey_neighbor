b43898eb76384634edb7a07935bdfd7f6a9f1e3d
security PIlVlP <the.security.pimp@gmail.com>
2020-01-20 15:27:45 -0700

    Added Port Option to Nikto Scans

--- nikto.py
+++ nikto.py
@@ -3,11 +3,11 @@
 import sys, os, time
 target = str(sys.argv[1])
 
-def nikto_http():
-	cmd4 = 'nikto -host ' + target + ' -port 80 -evasion 5 |tee nikto.txt'
+def nikto_http(http_port):
+	cmd4 = 'nikto -host ' + target + ' -port '+ http_port +' -evasion 5 |tee nikto.txt'
 	os.system(cmd4)
 
-def nikto_https():
-	cmd5 = 'nikto -host ' + target + ' -port 443 -evasion 5 |tee nikto.txt'
+def nikto_https(https_port):
+	cmd5 = 'nikto -host ' + target + ' -port '+ https_port +' -evasion 5 |tee nikto.txt'
 	os.system(cmd5)
 
