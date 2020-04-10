792736fce2d79e606283f750bce02a413d1f5975
security PIlVlP <the.security.pimp@gmail.com>
2020-04-10 17:38:39 -0600

    Update dirb.py

--- dirb.py
+++ dirb.py
@@ -4,10 +4,18 @@ import sys, os, time
 target = str(sys.argv[1])
 
 def dirb_http(http_port):
-	cmd2 = 'dirb http://' + target + ':' + http_port + ' -r |tee dirb_http.txt'
-	os.system(cmd2)
+	cmd100 = 'dirb http://' + target + ':' + http_port + ' -r |tee dirb_http.txt'
+	cmd101 = 'gobuster dir -u http://' + target + http_port + ' -w /usr/share/wordlists/dirb/common.txt -q -n -e | tee gobuster.txt
+	cmd102 = 'eyewitness -f gobuster.txt -d eyewitness --web --no-prompt'
+	os.system(cmd100)
+	os.system(cmd101)
+	os.system(cmd102)
 
 def dirb_https(https_port):
-	cmd3 = 'dirb https://' + target + ':' + https_port + ' -r |tee dirb_https.txt'
-	os.system(cmd3)
+	cmd150 = 'dirb https://' + target + ':' + https_port + ' -r |tee dirb_https.txt'
+	cmd151 = 'gobuster dir -u https://' + target + https_port + ' -w /usr/share/wordlists/dirb/common.txt -q -n -e | tee gobuster.txt
+	cmd152 = 'eyewitness -f gobuster.txt -d eyewitness --web --no-prompt'
+	os.system(cmd150)
+	os.system(cmd151)
+	os.system(cmd152)
 
